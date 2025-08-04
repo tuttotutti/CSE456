caa200c96daf06a777496ae6b571dce5719c8f38
tuttotutti <165403211+tuttotutti@users.noreply.github.com>
2025-08-04 22:46:06 +0700

    Delete Lab5-LeThiMinhTuoi-cse456 directory

--- Lab5-LeThiMinhTuoi-cse456/src/main/java/vn/edu/eiu/cse456/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.eiu.lab5;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("vn.edu.eiu.cse456")
-public class AppConfig {
-}
